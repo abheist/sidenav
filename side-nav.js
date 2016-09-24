eccd5f6ebb8d7798c2d2d57f15067efa10a1cffd
Abhishek <abhishek@kisanhub.com>
2016-09-24 19:00:57 +0530

    basic side nav completedf

--- side-nav.js
+++ side-nav.js
@@ -1,21 +1,28 @@
 'use strict'
 
 class SideNav {
-    constructor () {
-        this.toggleButtonEl = document.querySelector('.js-menu');
+    constructor() {
+        this.showButtonEl = document.querySelector('.js-menu-show');
+        this.hideButtonEl = document.querySelector('.js-menu-hide');
         this.SideNavEl = document.querySelector('.js-side-nav');
 
         this.showSideNav = this.showSideNav.bind(this);
+        this.hideSideNav = this.hideSideNav.bind(this);
 
         this.addEventListeners();
     }
 
-    addEventListeners () {
-        this.toggleButtonEl.addEventListener('click', this.showSideNav);
+    addEventListeners() {
+        this.showButtonEl.addEventListener('click', this.showSideNav);
+        this.hideButtonEl.addEventListener('click', this.hideSideNav);
     }
 
-    showSideNav () {
+    showSideNav() {
         this.SideNavEl.classList.add('side-nav--visible');
+    }
+
+    hideSideNav() {
+        this.SideNavEl.classList.remove('side-nav--visible');
     }
 }
 
